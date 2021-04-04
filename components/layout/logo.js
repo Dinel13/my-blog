5817e42ec7b0b830396b8eb98cc19a50583ade9e
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-04-04 11:24:50 +0800

    adding layout dan navigation

---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+export default function logo() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ components/layout/main-navigation.js
@@ -0,0 +1,23 @@
+import LInk from "next/link";
+import Logo from "./logo";
+
+export default function MainNavigation() {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

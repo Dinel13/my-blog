a7f0a6ac4dfd077d95b52d56f7551d6a92a45bdd
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-04-04 22:37:47 +0800

    adding function to read and fetch data markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 import PostHeader from "./post-header";
 
 import classes from "./post-content.module.css";
@@ -15,7 +17,7 @@ export default function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_DATA.title} image={imagePath} />
-      {DUMMY_DATA.content}
+      <ReactMarkdown>{DUMMY_DATA.content}</ReactMarkdown>
     </article>
   );
 }

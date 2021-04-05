dc2c51efe8d901195f2353a2f8b4c5b48ffe170a
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-04-05 15:32:40 +0800

    resize build file with optimize third party

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,11 +1,15 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript"
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css"
 
 import PostHeader from "./post-header";
-
 import classes from "./post-content.module.css";
+
+SyntaxHighlighter.registerLanguage('js', js) //param 1 diambil dari file markdownnya
+SyntaxHighlighter.registerLanguage('css', css) //jika file css maka merender gaya css
 
 export default function PostContent({ post }) {
   //to render img from markdown to IMage component fron next
